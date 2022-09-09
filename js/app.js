3fb1c02423c811b19c6e0c1e018490a2d721d7c1
unknown <shafaes@code.edu.az>
2022-09-09 09:46:27 +0400

    js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// function myFunction() {
+   // let text;
+// if (confirm("Press a button!") == true) {
+//   text = "You pressed OK!";
+// } else {
+//   text = "You canceled!";
+// } 
+// }
+
+
+// function myFunction() {
+//     let person = prompt("Please enter your age");
+//     if (person < 18) {
+//         let person = prompt("Sorry, your age is not appropriate");
+//     } 
+//     }
+ 
+function Groups(){ 
+    var value = document.getElementById('inp').value
+    var x = value(slice(-3))
+    if (x==1){
+     x.style.backgroundColor = "yellow"
+    }
+     else if (x==2){
+        x.style.backgroundColor = "orange"
+     }
+
+     else if (x==3){
+        x.style.backgroundColor = "blue"
+     } 
+    }
+    
+
+  
